82094fbeb11c38316e8a251b5607d8a5f51d9f42
Kasey Moffat <kasey@bitmatica.com>
2020-05-29 17:48:26 -0700

    hardcode image

--- shared-dev/blogmatica/main.tf
+++ shared-dev/blogmatica/main.tf
@@ -33,5 +33,5 @@ module "app" {
   db_username = var.db_username
   name = var.name
   environment = var.name
-  image = data.aws_ecr_image.image
+  image = "636934759355.dkr.ecr.us-east-1.amazonaws.com/nest-blogmatica:c506581bf1880f27c51ad494193579e03521267b"
 }
