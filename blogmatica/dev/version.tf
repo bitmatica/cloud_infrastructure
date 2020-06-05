8dc6863e6f9c81043d4421711b0ddd616eb8f9c4
Kasey Moffat <kasey@bitmatica.com>
2020-06-05 11:10:00 -0700

    rename

--- blogmatica/dev/version.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  backend_image = "636934759355.dkr.ecr.us-east-1.amazonaws.com/nest-blogmatica:0a8f3d4841b75eba36e38aa7a06dd40abc75f92c"
-  frontend_version = "v0.5.0"
-}
---
+++ blogmatica/dev/versions.tf
@@ -0,0 +1,4 @@
+locals {
+  backend_image = "636934759355.dkr.ecr.us-east-1.amazonaws.com/nest-blogmatica:8dea1e9bafa20a546ff51d78d1bc0f25787b8111"
+  frontend_version = "v0.5.0"
+}
