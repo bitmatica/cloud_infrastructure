c293ca176d957a20f16d71ede9556358c22e76d9
Kasey Moffat <kasey@bitmatica.com>
2020-06-15 13:16:53 -0700

    get versions from file

--- blogmatica/dev/versions.tf
+++ blogmatica/dev/versions.tf
@@ -1,4 +1,4 @@
 locals {
-  backend_image = "636934759355.dkr.ecr.us-west-2.amazonaws.com/blogmatica:c9dfc5aa7c1f8714a57859c45299d9848788c8c7"
-  frontend_version = "v0.1.0"
+  backend_image = file("${path.module}/backend_version.txt")
+  frontend_version = file("${path.module}/frontend_version.txt")
 }
